f4567d64a266c2628b64fd0da37d4ec52783f24e
Justyna Malinowska <justynamalinowska758@gmail.com>
2023-03-05 17:36:44 +0100

    Update RankedGradeBook's CalculateStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -58,7 +58,7 @@ namespace GradeBook.GradeBooks
 
         public override void CalculateStatistics()
         {
-            if (Students.Count < 5)
+            if (Students ==null || Students.Count() < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students.");
             }
@@ -70,7 +70,7 @@ namespace GradeBook.GradeBooks
 
         public override void CalculateStudentStatistics(string name)
         {
-            if (Students.Count < 5)
+            if (Students.Count() < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students.");
             }
