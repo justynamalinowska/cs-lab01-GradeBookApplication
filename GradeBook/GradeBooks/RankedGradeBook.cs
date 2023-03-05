c43fb4779a297f1875b3141479bbf5865087fc34
Justyna Malinowska <justynamalinowska758@gmail.com>
2023-03-05 13:33:30 +0100

    Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Xml.Linq;
 using GradeBook.Enums;
 
@@ -10,6 +11,49 @@ namespace GradeBook.GradeBooks
         {
 			Type = GradeBookType.Ranked;
         }
-	}
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            //int counter = 0;
+            int N = (int)Math.Ceiling(0.2 * Students.Count);
+            var sortedAverages = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
+
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Program requires a minimum 5 students");
+            }
+
+            for (int i = 0; i < sortedAverages.Count; i += N)
+            {
+                if (i + N > sortedAverages.Count)
+                {
+                    N = sortedAverages.Count - i;
+                }
+
+                if (averageGrade >= sortedAverages[i])
+                {
+                    return 'A';
+                }
+                else if (averageGrade >= sortedAverages[i + N])
+                {
+                    return 'B';
+                }
+                else if (averageGrade >= sortedAverages[i + (N * 2)])
+                {
+                    return 'C';
+                }
+                else if (averageGrade >= sortedAverages[i + (N * 3)])
+                {
+                    return 'D';
+                }
+                else if (averageGrade >= sortedAverages[i + (N * 4)])
+                {
+                    return 'F';
+                }  
+            }
+            return 'F';
+        }
+
+    }
 }
 
