053dfefbb46538c76a367db35835bda29d9a4b0d
Justyna Malinowska <justynamalinowska758@gmail.com>
2023-03-05 12:24:16 +0100

    Creating the RankedGradeBook class

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Xml.Linq;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+	public class RankedGradeBook : BaseGradeBook
+    {
+		public RankedGradeBook(string name) : base(name)
+        {
+			Type = GradeBookType.Ranked;
+        }
+	}
+}
+
--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -5,9 +5,6 @@ namespace GradeBook.GradeBooks
 {
 	public class StandardGradeBook : BaseGradeBook
 	{
-		public string Name { get; set; }
-		public GradeBookType Type { get; set; }
-
 		public StandardGradeBook(string name) : base(name)
 		{
 			Type = GradeBookType.Standard;
